42d4d637f6a3f72f9868176144e7f3570bf80b8b
SAMEER KULKARNI <sunbird.sameer@gmail.com>
2023-04-30 13:51:56 +0530

    Update Ultrasound_Ground_Clearance_Data_to_CSV.py

--- Ultrasound_Ground_Clearance_Data_to_CSV.py
+++ Ultrasound_Ground_Clearance_Data_to_CSV.py
@@ -1,9 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 import csv
-import os
-
-os.system("rm *.csv")
+import datetime
 
 # Set up GPIO pins
 GPIO.setmode(GPIO.BCM)
@@ -12,41 +10,44 @@ ECHO_PIN = 24
 GPIO.setup(TRIG_PIN, GPIO.OUT)
 GPIO.setup(ECHO_PIN, GPIO.IN)
 
-FILENAME = 'distance_data.csv'
+FILENAME = datetime.datetime.now().strftime("%y_%m_%d_%H_%M_%S") + '_distance_data.csv'
 last_distance = None
 
-while True:
-    # Trigger the ultrasonic sensor to send out a pulse
-    GPIO.output(TRIG_PIN, True)
-    time.sleep(0.00001)
-    GPIO.output(TRIG_PIN, False)
+# Open the CSV file for writing
+with open(FILENAME, 'w', newline='') as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerow(['distance'])
 
-    # Measure the time it takes for the pulse to bounce back
-    while GPIO.input(ECHO_PIN) == 0:
-        pulse_start = time.time()
-    while GPIO.input(ECHO_PIN) == 1:
-        pulse_end = time.time()
+    while True:
+        # Trigger the ultrasonic sensor to send out a pulse
+        GPIO.output(TRIG_PIN, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG_PIN, False)
 
-    # Calculate the distance in centimeters
-    pulse_duration = pulse_end - pulse_start
-    distance = pulse_duration * 17150
-    distance = round(distance, 2)
+        # Measure the time it takes for the pulse to bounce back
+        while GPIO.input(ECHO_PIN) == 0:
+            pulse_start = time.time()
+        while GPIO.input(ECHO_PIN) == 1:
+            pulse_end = time.time()
 
-    # Filter out invalid measurements
-    if distance <= 50:
-        # Discard readings that are more than 5 cm away from the last valid reading
-        if last_distance is None or abs(distance - last_distance) <= 5:
-            # Write the distance measurement to a CSV file
-            with open(FILENAME, 'a', newline='') as csv_file:
-                writer = csv.writer(csv_file)
+        # Calculate the distance in centimeters
+        pulse_duration = pulse_end - pulse_start
+        distance = pulse_duration * 17150
+        distance = round(distance, 2)
+
+        # Filter out invalid measurements
+        if distance <= 50:
+            # Discard readings that are more than 5 cm away from the last valid reading
+            if last_distance is None or abs(distance - last_distance) <= 5:
+                # Write the distance measurement to the CSV file
                 writer.writerow([distance])
 
-            # Print the distance measurement to the console
-            print(f'Distance: {distance} cm')
-            
-            # Update the last valid distance
-            last_distance = distance
-    else:
-        pass
-    
-    time.sleep(0.1)
+                # Print the distance measurement to the console
+                print(f'Distance: {distance} cm')
+
+                # Update the last valid distance
+                last_distance = distance
+        else:
+            pass
+
+        time.sleep(0.002)
